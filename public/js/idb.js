f0fbd4fbefeec8d61d930cc3fcd0f95171035555
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-22 18:54:22 -0500

    added Offline Persistence with IndexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -14,9 +14,9 @@ request.onsuccess = function(event) {
   // When the database is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
-  // Check if app is online; if yes run sendTransaction(); function to send all local db data to the API.
+  // Check if app is online; if yes run uploadTransaction(); function to send all local db data to the API.
   if (navigator.onLine) {
-    sendTransaction();
+    uploadTransaction();
   }
 };
 
@@ -30,9 +30,56 @@ function saveRecord(record) {
   // open a new transaction with the database with read and write permissions 
   const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-  // access the object store for `new_pizza`
+  // access the object store for `new_transaction`
   const transactionObjectStore = transaction.objectStore('new_transaction');
 
   // add record to your store with add method
   transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access your object store
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  // Retrieve all records from store and set to a variable.
+  const getAll = transactionObjectStore.getAll(); // getAll is an asynchronous function to which we must to attach an event handler in order to retrieve the data.
+
+  // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+  // if there was data in indexedDb's store, let's send it to the api server
+  if (getAll.result.length > 0) {
+    fetch('/api/transaction', {
+      method: 'POST',
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(serverResponse => {
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+        // open one more transaction
+        const transaction = db.transaction(['new_transaction'], 'readwrite');
+        // access the new_transaction object store
+        const transactionObjectStore = transaction.objectStore('new_transaction');
+        // clear all items in your store
+        transactionObjectStore.clear();
+
+        alert('All saved transactions have been submitted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+}
+
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
